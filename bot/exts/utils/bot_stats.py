90715034cc139e4bccb2d8464a83e62f4fc23a63
Arnav-2004 <arnavrangwani@gmail.com>
2022-01-10 12:02:30 +0530

    add slash commands for bot stats

--- bot/exts/utils/bot_stats.py
+++ bot/exts/utils/bot_stats.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from platform import python_version
 
 import humanize
-from disnake import Embed, __version__
+from disnake import ApplicationCommandInteraction, Embed, __version__
 from disnake.ext import commands
 
 from bot.bot import Bot
@@ -15,19 +15,14 @@ class BotStats(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    @commands.command()
-    async def ping(self, ctx: commands.Context) -> None:
-        """Ping the bot to see its latency."""
-        embed = Embed(
+    async def _make_ping_embed(self) -> Embed:
+        return Embed(
             title="Pong!",
             description=f"Gateway Latency: {round(self.bot.latency * 1000)}ms",
             color=Colors.green,
         )
-        await ctx.reply(embed=embed)
 
-    @commands.command()
-    async def stats(self, ctx: commands.Context) -> None:
-        """Get the information and current uptime of the bot."""
+    async def _make_stats_embed(self) -> Embed:
         embed = Embed(
             title="Bot Stats",
             color=Colors.green,
@@ -47,7 +42,28 @@ class BotStats(commands.Cog):
 
         for name, value in list(fields.items()):
             embed.add_field(name=name, value=value, inline=False)
-        await ctx.reply(embed=embed)
+
+        return embed
+
+    @commands.command(name="ping")
+    async def ping(self, ctx: commands.Context) -> None:
+        """Ping the bot to see its latency."""
+        await ctx.reply(embed=await self._make_ping_embed())
+
+    @commands.slash_command(name="ping")
+    async def slash_ping(self, inter: ApplicationCommandInteraction) -> None:
+        """Ping the bot to see its latency."""
+        await inter.send(embed=await self._make_ping_embed())
+
+    @commands.command(name="stats")
+    async def stats(self, ctx: commands.Context) -> None:
+        """Get the information and current uptime of the bot."""
+        await ctx.reply(embed=await self._make_stats_embed())
+
+    @commands.slash_command(name="stats")
+    async def slash_stats(self, inter: ApplicationCommandInteraction) -> None:
+        """Get the information and current uptime of the bot."""
+        await inter.send(embed=await self._make_stats_embed())
 
 
 def setup(bot: Bot) -> None:
